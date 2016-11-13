113040232127b3bcd9966f78dd3e795438b67cdf
Peter Den Hartog <pddenhar@gmail.com>
2016-11-13 16:24:04 -0600

    Refactored MRPC into its own independent Android module that can be used in other applications

---
+++ mrpc/src/main/java/net/vector57/android_mrpc/Message.java
@@ -0,0 +1,58 @@
+package net.vector57.android_mrpc;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+
+/**
+ * Created by Vector on 11/12/2016.
+ */
+
+public abstract class Message {
+    private static Gson _gson;
+    public int id;
+    public String src;
+    public String dst;
+
+    public static Gson gson() {
+        if(_gson == null) {
+            GsonBuilder builder = new GsonBuilder();
+            builder.registerTypeAdapter(Message.class, new MessageTypeAdapter());
+            _gson = builder.create();
+        }
+        return _gson;
+    }
+
+    public static class Request extends Message {
+        public JsonElement value;
+        public Request(int id, String src, String dst, Object value) {
+            super(id, src, dst);
+            this.value = gson().toJsonTree(value);
+        }
+    }
+    public static class Response extends Message {
+        public Response(int id, String src, String dst) {
+            super(id, src, dst);
+        }
+        public JsonElement result;
+        public JsonElement error;
+    }
+
+    public Message(int id, String src, String dst) {
+        this.id = id;
+        this.src = src;
+        this.dst = dst;
+    }
+
+    public static Message FromJson(String string) {
+        return gson().fromJson(string, Message.class);
+    }
+
+    public String toJSON() {
+        return gson().toJson(this);
+    }
+
+    public Boolean isValid() {
+        return id != 0 && src != null && dst != null;
+    }
+}
---
+++ mrpc/src/main/java/net/vector57/android_mrpc/Result.java
@@ -0,0 +1,35 @@
+package net.vector57.android_mrpc;
+
+import android.os.Handler;
+
+import com.google.gson.JsonElement;
+
+/**
+ * Created by Vector on 11/12/2016.
+ */
+
+public class Result implements Runnable{
+    public static abstract class Callback {
+        public void onSuccess(JsonElement value) {};
+        public void onFailure(JsonElement value) {};
+    }
+    private boolean success;
+    private JsonElement value;
+    public Callback callback;
+    public Result(Callback callback) {
+        this.callback = callback;
+    }
+    public void resolve(Handler handler, JsonElement value, boolean success) {
+        this.success = success;
+        this.value = value;
+        handler.post(this);
+    }
+
+    @Override
+    public void run() {
+        if(success)
+            this.callback.onSuccess(value);
+        else
+            this.callback.onFailure(value);
+    }
+}
